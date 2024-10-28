da02bd977a2c992d85453f49fcf30bc87bbcb246
Pranav Gupta <pranav04g@gmail.com>
2024-10-27 19:13:15 -0700

    all files were uploaded by 10/24/24; this is an update for visibility

--- frontend1/App.js
+++ frontend1/App.js
@@ -1,4 +1,4 @@
 //some code files (such as machine learning models) are too large for GitHub and require the use of GitHub LFS
 //some code files not added for privacy reasons (ex. API key, etc.)
 
-//all code files as shown in presentation video are on Pranav Gupta's computer; feel free to reach out for them: pranav04g@gmail.com
+//all code files as shown in presentation video are on Pranav Gupta's computer; please reach out for them: pranav04g@gmail.com
