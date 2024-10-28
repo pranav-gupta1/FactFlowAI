389e1d2542eff3130877bbce437270e126f20078
Pranav Gupta <pranav04g@gmail.com>
2024-10-27 19:06:46 -0700

    add files

--- frontend1/App.js
+++ frontend1/App.js
@@ -1,28 +1,4 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/NavBar';
-import FooterBar from './components/FooterBar';
-import Home from './components/Home';
-import Hub from './components/Hub';
-import Welcome from './components/Welcome';
-import './App.css';
+//some code files (such as machine learning models) are too large for GitHub and require the use of GitHub LFS
+//some code files not added for privacy reasons (ex. API key, etc.)
 
-function App() {
-  const [language, setLanguage] = useState('en');
-
-  return (
-    <Router>
-      <div className="App">
-        <Navbar language={language} setLanguage={setLanguage} />
-        <Routes>
-          <Route path="/home" element={<Home language={language} />} />
-          <Route path="/hub" element={<Hub language={language} />} />
-          <Route path="/" element={<Welcome language={language} />} />
-        </Routes>
-        <FooterBar />
-      </div>
-    </Router>
-  );
-}
-
-export default App;
+//all code files as shown in presentation video are on Pranav Gupta's computer; feel free to reach out for them: pranav04g@gmail.com
